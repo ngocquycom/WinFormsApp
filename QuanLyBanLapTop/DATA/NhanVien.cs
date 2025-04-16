bc72f2414aa928d8cce3b3df1ac23e4832b81715
ngocquycom <ngocq1665@gmail.com>
2025-04-16 15:19:05 +0700

    demo sql

---
+++ QuanLyBanLapTop/DATA/NhanVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanLapTop.DATA
+{
+    class NhanVien
+    {
+        public int ID { get; set; }
+        public string HoVaTen { get; set; } = null!;
+        public string? DienThoai { get; set; }
+        public string? DiaChi { get; set; }
+        public string TenDangNhap { get; set; } = null!;
+        public string MatKhau { get; set; } = null!;
+        public string QuyenHan { get; set; } = null!; // admin, user
+        public virtual ICollection<HoaDon> HoaDon { get; } = new List<HoaDon>();
+    }
+}
